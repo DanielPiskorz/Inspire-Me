d19e428d4afd1cdafff84a32cc5042d4a3465942
DanielPiskorz <eel5426@gmail.com>
2018-10-17 20:59:38 +0200

    Http quote service

---
+++ src/app/services/quote-http.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Quote } from 'src/app/models/Quote';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuoteHttpService {
+
+  _url = 'https://favqs.com/api/qotd';
+
+  constructor(private http: HttpClient) { }
+
+  getQuote(): Observable<Quote> {
+    return this.http.get<Quote>(this._url);
+  }
+}
