432f3c07f6ef740468da74842e42a626a5c0f884
DanielPiskorz <eel5426@gmail.com>
2018-10-26 21:17:10 +0200

    Content fade in animation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import { ContentService } from 'src/app/services/content.service';
+import { animate, style, trigger, transition } from '@angular/animations';
 
 import { Content } from '../models/Content';
 import { SimpleContent } from '../impl/SimpleContent';
@@ -8,7 +9,15 @@ import { SimpleContent } from '../impl/SimpleContent';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  animations:  [
+    trigger('contentFadeIn', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(2000, style({opacity: 100}))
+      ])
+    ])
+  ]
 })
 export class MainComponent {
 
