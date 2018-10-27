6508ce6673d3d5e7a5ba51043ad89df6d14aafa7
DanielPiskorz <eel5426@gmail.com>
2018-10-27 17:22:57 +0200

    Pixabay request image resolution params

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   PIXABAY_API_URL: 'https://pixabay.com/api/',
   PIXABAY_KEY: '5943798-876ea09dff1de2324ea1ec7bd',
   PIXABAY_QUERY: 'mountains',
-  PIXABAY_PARAMS: 'image_type=photo&pretty=true',
+  PIXABAY_PARAMS: 'image_type=photo&pretty=true&min_width=1500&min_height=1000',
   MAX_IMAGES: 20 // <3, 200>
 };
 
